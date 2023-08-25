fb401dfcae3530aa8f86251fd80fe87981d99a8d
niyatipatel97 <patelniyati97@gmail.com>
2023-08-25 10:13:26 +0200

    Final commit with changes in important files.

--- Pods/ESPullToRefresh/Sources/ES.swift
+++ Pods/ESPullToRefresh/Sources/ES.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-public protocol ESExtensionsProvider: class {
+public protocol ESExtensionsProvider: AnyObject {
     associatedtype CompatibleType
     var es: CompatibleType { get }
 }
